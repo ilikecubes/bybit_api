f4bc1cd08e144db7eb3196c9818e1f0021159886
Kevin <scheeren@proton.me>
2022-12-06 16:15:51 +0100

    Initial Commit

---
+++ bin/bybit_api.dart
@@ -0,0 +1,5 @@
+import 'package:bybit_api/bybit_api.dart' as bybit_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bybit_api.calculate()}!');
+}
---
+++ test/bybit_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:bybit_api/bybit_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
